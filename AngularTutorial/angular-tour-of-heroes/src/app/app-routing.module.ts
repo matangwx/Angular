6fafda6aef1d5431cbda863b524e8cee13c3e2b1
Matthew <matth3w_wx@hotmail.com>
2019-01-20 22:33:34 +0800

    Your Message

--- AngularTutorial/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ AngularTutorial/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'hero-detail', component: HeroDetailComponent }
 ];
 
 
