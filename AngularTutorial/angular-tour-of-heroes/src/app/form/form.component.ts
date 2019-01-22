e1a1ad1102b99b594c0c1b2a83473878d22b57ee
MatthewTang <matth3w_wx@hotmail.com>
2019-01-22 11:15:54 +0800

    Remove un-use import

--- AngularTutorial/angular-tour-of-heroes/src/app/form/form.component.ts
+++ AngularTutorial/angular-tour-of-heroes/src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormArray } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 
 @Component({
