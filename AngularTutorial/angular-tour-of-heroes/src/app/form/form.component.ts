6ccf84febeac8ed1988724ceab7326a7dd896dca
MatthewTang <matth3w_wx@hotmail.com>
2019-01-22 18:47:19 +0800

    Remove un-use import

--- AngularTutorial/angular-tour-of-heroes/src/app/form/form.component.ts
+++ AngularTutorial/angular-tour-of-heroes/src/app/form/form.component.ts
@@ -28,7 +28,6 @@ export class FormComponent implements OnInit {
     this.myForm.valueChanges.subscribe(val => {
       this.jsonString = this.myForm.value;
     });
-
   }
 
   createItem(): FormGroup {
